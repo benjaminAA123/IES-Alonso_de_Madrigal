5fd513c15cc54801b8612b2e558b93dd9e90c582
benjamin <benjamin.alaama@educa.jcyl.es>
2023-02-09 02:58:39 +0100

    clases + main a medias

--- src/com/iesam/AlonsoDeMadrigal/presentation/Main.java
+++ src/com/iesam/AlonsoDeMadrigal/presentation/Main.java
@@ -1,10 +1,72 @@
 package com.iesam.AlonsoDeMadrigal.presentation;
 
+import com.iesam.AlonsoDeMadrigal.domain.models.*;
+
 public class Main {
     public static void main(String[] args) {
 
 
-        System.out.println("Hello world!");
+
+        Course course = new Course();
+        System.out.println("Course");
+        course.setId("1");
+        course.setName("A1");
+        course.setDescription("SMR");
+
+        Subject subject = new Subject();
+        System.out.println("Subject");
+        subject.setId("001");
+        subject.setName("Matematicas ");
+
+
+        Registration registration = new Registration();
+        System.out.println("Registration");
+        registration.setId("10");
+        registration.setDescription("Sistemas Operativo Monopuestos ");
+
+
+
+
+        Pupil pupil = new Pupil();
+        System.out.println("Pupil");
+        pupil.setId("1001");
+        pupil.setName("Federico");
+        pupil.setSurname("Alevin");
+        pupil.setDni("7897656F");
+        pupil.setAdress("Ávila");
+        pupil.setMother("Maria ");
+        pupil.setFather("Jose");
+        pupil.setEmail("feder1c0@gmail.com");
+
+
+        Teacher teacher = new Teacher();
+        System.out.println("Teacher");
+        teacher.setId("101");
+        teacher.setName("Noel");
+        teacher.setSurname("Rodriguez");
+        teacher.setDni("54654654R");
+        teacher.setAdress("Zaragoza");
+        teacher.setAcademicTitle("Informatica");
+        teacher.setRegistDate("20/15/2020");
+
+
+
+        Classroom classroom = new Classroom();
+
+        TeachingRoom teachingRoom = new TeachingRoom();
+        teachingRoom.setId("1");
+        teachingRoom.setFloor("2");
+        teachingRoom.setChairNum(15);
+        teachingRoom.setCapacity("15");
+        teachingRoom.setBoardType("tactile");
+
+
+        PcClassroom pcClassroom = new PcClassroom();
+        pcClassroom.setId("2");
+        pcClassroom.setFloor("3");
+        pcClassroom.setPcNumber(01);
+        pcClassroom.setCapacity("30");
+        pcClassroom.setPcType("destop");
 
 
     }
